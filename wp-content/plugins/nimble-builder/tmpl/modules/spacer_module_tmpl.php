3aa4fbc84bd9b466dccdc54335c7e4b2a8bf0e16
tmoigne <71131533+theomoigne@users.noreply.github.com>
2020-12-09 11:18:38 +0100

    save_1

--- wp-content/plugins/nimble-builder/tmpl/modules/spacer_module_tmpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Developers : you can override this template from a theme with a file that has this path : 'nimble_templates/modules/{original-module-template-file-name}.php' */
-if ( !defined( 'ABSPATH' ) ) {
-    exit;
-}
-
-echo '<div class="sek-spacer"></div>';
